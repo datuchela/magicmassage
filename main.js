152ac7615df74ed3dd180eb1df83969e4e0e09d0
datuchela <davit.narimanidze408@gmail.com>
2023-01-13 21:01:31 +0400

    changed some comments in js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Used no JS, since there was no description provided for extra functionalities.
 // I would rather not implement anything that is not provided to me, than waste my time and energy on it.
-// While unexpectedly implemented features could be nice to a company, it also introduces inconsistency.
+// While "surprise" features could be nice, it also introduces inconsistency.
 // Problem could be solved by providing more detailed description about the assignment and/or
 // better design file that showcases states like button hovered state, focused etc.
